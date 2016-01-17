3e1678dc7efbe0e4d2fc8f17963feb5e7a85bb78
kelukelugames <kelukelugames@gmail.com>
2016-01-17 08:52:03 -0800

    Add sound icon

--- scripts.js
+++ scripts.js
@@ -1,14 +1,39 @@
-var words = ["abc", "xyz", "123"];
+var goodWords = ["tyrant", "tyranny", "tyrannical"];
+var badWords = ["yogurt", "moat", "gopher", "moped", "twelve", "fish"];
 
 $(document).ready(function() {
-
 	var table = document.getElementById("wordsTable");
+	var maxArrayLength = Math.max(goodWords.length, badWords.length)
 
-	for (var i = 0; i < words.length; i++) {
-		var row = table.insertRow(0);
+	for (var i = 0; i < maxArrayLength; i++) {
+		var row = table.insertRow(i);
 
 		var cell1 = row.insertCell(0);
+		cell1.classList.add("good");
+		var cell2 = row.insertCell(1);
+		cell1.classList.add("bad");
 
-		cell1.innerHTML = words[i];
+		cell1.innerHTML = (i < goodWords.length) ? createHTML(goodWords[i]) : "";
+		cell2.innerHTML = (i < badWords.length) ? createHTML(badWords[i]) : "";
 	}
 });
+
+function createHTML(word) {
+	return "<div>" + word +
+				" <img src='soundicon.png'" +
+					"onClick='playSound(\"" + word + "\")'>" +
+			"</div>";
+}
+
+function playSound(word) {
+	var audio = new Audio("https://ssl.gstatic.com/dictionary/static/sounds/de/0/" + word + ".mp3");
+audio.play();
+}
+//
+
+/*
+div on click 
+get ids of div
+calls a funcion
+
+*/
