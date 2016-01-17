e0bdbea40d390edd57f1c4cae7254e2f750d1982
kelukelugames <kelukelugames@gmail.com>
2016-01-17 10:26:20 -0800

    shuffle good words list

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
 	var goodWords = readWordsFromFilepath ("https://raw.githubusercontent.com/kelukelugames/pronounceenglishwords/master/goodwords.txt");
+	goodWords = shuffleArray(goodWords);
 	var badWords = readWordsFromFilepath("https://raw.githubusercontent.com/kelukelugames/pronounceenglishwords/master/badwords.txt");
 
 	var table = document.getElementById("wordsTable");
@@ -38,8 +39,10 @@ function createHTML(word) {
 
 function playSound(word) {
 	var audio = new Audio("https://ssl.gstatic.com/dictionary/static/sounds/de/0/" + word + ".mp3");
-audio.play();
+	debugger;
+	audio.play();
 }
+
 function readWordsFromFilepath(filepath) {
     var request = new XMLHttpRequest();
     request.open("GET", filepath, false);
@@ -47,5 +50,16 @@ function readWordsFromFilepath(filepath) {
     var returnValue = request.responseText.trim().split("\n");
 
     return returnValue;
+}
+
+function shuffleArray(words) {
+	for (var i = 1; i < words.length; i++) {
+		var temp = words[i];
+		var swapIndex = Math.floor(Math.random() * (i + 1));
+		words[i] = words[swapIndex];
+		words[swapIndex] = temp;
+	}
+
+	return words;
 }
 
